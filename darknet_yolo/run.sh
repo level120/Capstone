0071c999fd220c959b25968ac09038a57f53f086
Gusam Park <tnara9573@naver.com>
2018-05-02 21:06:15 +0900

    add django

--- darknet_yolo/run.sh
+++ darknet_yolo/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./darknet yolo demo cfg/coco.data cfg/tiny-yolo.cfg yolov2-tiny.weights rtsp://210.119.85.77:1554/video1 -i 0# -thresh 0.7# -out_filename res.mp4
+./darknet yolo demo cfg/tiny-yolo.cfg yolov2-tiny.weights rtsp://210.119.85.77:1554/video1 -i 0# -thresh 0.7# -out_filename res.mp4
 #./darknet yolo demo cfg/yolov2.cfg yolov2.weights rtsp://210.119.85.77:1554/video1 -i 0# -out_filename res.mp4
 #./darknet yolo demo cfg/yolov2.cfg yolov2.weights /home/server/Downloads/1.mp4
 #./darknet yolo demo cfg/yolov2.cfg yolov2.weights /home/server/Downloads/2.mp4
